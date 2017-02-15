88a5458c045134f36c7953a13e8bab3511fa26a0
lisai <995881161@qq.com>
2017-02-15 17:29:15 +0800

    Initial Commit

---
+++ LiveSlideDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveSlideDemo
+//
+//  Created by 李赛 on 17/2/15.
+//  Copyright © 2017年 Sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LiveSlideDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LiveSlideDemo
+//
+//  Created by 李赛 on 17/2/15.
+//  Copyright © 2017年 Sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
